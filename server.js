defeb203b7f1ad8664313b51e8b7e924aba28b6c
jchinwor <jchinwor@gmail.com>
2025-08-23 14:02:19 +0000

    added dotenv

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 
 connectDB();
+require('dotenv').config();
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
