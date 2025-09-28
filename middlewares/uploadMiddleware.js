1dc2d018dc8590c309f1c2d5e2c1f8ea2b68df0a
Jenkins Chinwor <90245242+jchinwor@users.noreply.github.com>
2025-09-28 22:11:54 +0000

    Update uploadMiddleware.js

--- middlewares/uploadMiddleware.js
+++ middlewares/uploadMiddleware.js
@@ -4,13 +4,15 @@ const path = require('path');
 // Storage configuration
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/foods'); // Save files inside /uploads/foods
+    const uploadPath = path.join(__dirname, '..', 'uploads', 'foods');
+    cb(null, uploadPath);
   },
   filename: (req, file, cb) => {
     const ext = path.extname(file.originalname);
     cb(null, `food-${Date.now()}${ext}`);
   }
 });
+
 
 // File filter (only images allowed)
 const fileFilter = (req, file, cb) => {
