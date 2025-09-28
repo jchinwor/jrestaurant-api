cd3484b2bdaef96a707be3fa79b0b6ddcd23bde8
Jenkins Chinwor <90245242+jchinwor@users.noreply.github.com>
2025-09-28 23:30:28 +0000

    Update debugRoutes.js

--- routes/debugRoutes.js
+++ routes/debugRoutes.js
@@ -1,1 +1,12 @@
+const fs = require('fs');
+const path = require('path');
 
+router.get('/uploads', (req, res) => {
+  const folderPath = path.join(process.cwd(), 'uploads', 'foods');
+  try {
+    const files = fs.readdirSync(folderPath);
+    res.json({ files });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
