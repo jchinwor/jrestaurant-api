28c7056a733c20b4ae6353d48069e78d19bd4cc9
Jenkins Chinwor <90245242+jchinwor@users.noreply.github.com>
2025-10-05 15:22:48 +0000

    Update orderRoutes.js

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,6 +1,6 @@
 // routes/orderRoutes.js
 const express = require("express");
-const { placeOrder, getMyOrders, getAllOrders } = require("../controllers/orderController");
+const { placeOrder, getMyOrders, getAllOrders, updateOrder } = require("../controllers/orderController");
 const { protect } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
@@ -10,5 +10,6 @@ router.use(protect);
 router.post("/place", placeOrder);
 router.get("/my-orders", getMyOrders);
 router.get("/all-orders", getAllOrders);
+router.patch("/update-order", updateOrder);
 
 module.exports = router;
