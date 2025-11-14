58efcfeb9765e284a66b69e7b04359672a96a50a
Jenkins Chinwor <90245242+jchinwor@users.noreply.github.com>
2025-11-14 23:13:57 +0000

    Remove verification code routes from userRoutes
    Removed unused routes for verification code handling.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,12 +7,11 @@ router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.post('/signout',protect, authController.signout);
 
-router.patch('/send-verification-code',protect, authController.sendVerificationCode);
-router.patch('/verify-verification-code',protect, authController.verifyVerificationCode);
+
 router.patch('/reset-password', authController.resetPassword);
 
 router.patch('/send-forgot-password', authController.forgotPassword);
-router.patch('/verify-forgot-password-code', authController.verifyForgotPasswordCode);
+
 
 
 router.get('/me', protect, authController.getMe);
